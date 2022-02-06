fd67b40d7242eb1f0aa379e1c864786a889a0dba
Pankajbisht18 <techpankaj45@gmail.com>
2022-02-06 22:34:24 +0530

    Complete API routes and Result Component

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import request from '../utils/request';
+import request from '../utils/requests';
 
 const Nav = () => {
     const router = useRouter();
@@ -11,7 +11,7 @@ const Nav = () => {
                     key={key} 
                     onClick={()=> router.push(`/?genre=${key}`)}
                     className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">
-                            {title}
+                        {title}
                     </h2>
                 ))}
             </div>
